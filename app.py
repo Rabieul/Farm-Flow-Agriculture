69f437b0e34851b0177e7788b004eaa60e9ce2b6
Rabieul <rabieulrahman.albi@gmail.com>
2025-05-10 18:11:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -232,5 +232,9 @@ def crop_suggestion():
 def powerbi():
     return render_template('powerbi.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host='0.0.0.0', port=port)
+
